181807af2c9cfd922771da3a12fe489b4daada62
Joey Conroy <Joey.Conroy@jhuapl.edu>
2021-07-01 12:49:07 -0400

    added views section

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import HeroImage from '../images/hero.webp'
 import Logo from '../images/big-logo.inline.svg'
 import "../styles.scss"
 import QuickLinks from "../components/QuickLinks";
+import Button from "../components/Button";
 
 
 const IndexPage = (props) => {
@@ -19,7 +20,7 @@ const IndexPage = (props) => {
       <section className="tastings">
         <StaticImage className="tastings-img" layout="fullWidth" src="../images/tastings.png" alt="tastings pic" />
         <div className="tastings__text">
-          <h2 className="">Tastings</h2>
+          <h2 className="spaced-bottom">Tastings</h2>
           <div className="text-body-regular light">
             <p>Wine Tasting at Virginia Mountain Vineyards brings together the best that
               Virginia has to offer  -  panoramic views of the Blue Ridge Mountains and
@@ -35,7 +36,7 @@ const IndexPage = (props) => {
       <section className="tours">
         <StaticImage className="tours-img" layout="fullWidth" src="../images/tours.webp" alt="tastings pic" />
         <div className={'tours__text'}>
-          <h2 className={'dark spaced-right'}>Tours</h2>
+          <h2 className={'dark spaced-right tours__header'}>Tours</h2>
           <div className={'tours__text-body'}>
             <div className={'text-body-regular tours__blurb'}>Learning about the mystery of winemaking is one of the unique pleasures of visiting a winery.  At Virginia Mountain Vineyards, we provide tours of our vineyard and winemaking processes to enhance the wine tasting experience and provide a glimpse into the magical journey of bringing science and art together to make great wine.</div>
             <Link className="tours__button" to="/tours/">learn more +</Link>
@@ -47,12 +48,26 @@ const IndexPage = (props) => {
           <div className={'weddings__photobook-frame'}></div>
         </div>
         <div className={'weddings__text'}>
-          <h2 className={'dark'}>Weddings at VMV</h2>
+          <h2 className={'dark spaced-bottom'}>Weddings at VMV</h2>
           <div className={'weddings__blurb text-body-regular'}>
             The natural beauty of Virginia Mountain Vineyards is what wedding dreams are made of. From an intimate wedding ceremony nestled in the grapevines to the glittering, vaulted wedding tent overlooking the panoramic view of the Blue Ridge Mountains, weddings at Virginia Mountain Vineyards are truly special and leave lasting memories for brides, grooms and their guests!
           </div>
           <Link className="weddings__button" to="/weddings/">learn more +</Link>
         </div>
+      </section>
+      <section className={'views'}>
+        <h1 className={'views__header center dark'}>Exceptional wines. Breathtaking views.</h1>
+        <div className={'views__buttons'}>
+          <Button
+            label="Our Story"
+            link="/about-us/"
+            hoverText="About us"
+            variant="primary" />
+          <Button
+            label="Contact Us"
+            link="/contact-us/"
+            hoverText="Contacr us"
+            variant="secondary" />          </div>
       </section>
     </main>
   )
