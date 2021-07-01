3082cbd1b9cacd445c7c11894585b770ba5a8b25
Joey Conroy <Joey.Conroy@jhuapl.edu>
2021-07-01 12:00:04 -0400

    added wedding stuff back

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,16 @@ const IndexPage = (props) => {
         </div>
       </section>
       <section className={'weddings'}>
-        <div className={'weddings__photobook'}></div>
+        <div className={'weddings__photobook'}>
+          <div className={'weddings__photobook-frame'}></div>
+        </div>
+        <div className={'weddings__text'}>
+          <h2 className={'dark'}>Weddings at VMV</h2>
+          <div className={'weddings__blurb text-body-regular'}>
+            The natural beauty of Virginia Mountain Vineyards is what wedding dreams are made of. From an intimate wedding ceremony nestled in the grapevines to the glittering, vaulted wedding tent overlooking the panoramic view of the Blue Ridge Mountains, weddings at Virginia Mountain Vineyards are truly special and leave lasting memories for brides, grooms and their guests!
+          </div>
+          <Link className="weddings__button" to="/tours/">learn more +</Link>
+        </div>
       </section>
     </main>
   )
