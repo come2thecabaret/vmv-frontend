9ff1bacfc02bd8fed6ca306bfdfab03b014209fd
Joey Conroy <Joey.Conroy@jhuapl.edu>
2021-07-02 14:32:16 -0400

    netlify function working

--- src/components/WineList.js
+++ src/components/WineList.js
@@ -1,50 +1,86 @@
 import React, { useState, useEffect } from 'react';
-
+import useVinoshipperApi from '../hooks/useVinoshipperApi';
 const WineList = (props) => {
+    const [vinoData, setVinoData] = useState([]);
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [vinoData, setVinoData] = useState([]);
-    // Note: the empty deps array [] means
-    // this useEffect will run once
-    // similar to componentDidMount()
+    //get vinoshipper data and load into state
     useEffect(() => {
-        fetch("/.netlify/functions/get-vinos", {
-            mode: 'no-cors'
-        })
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setVinoData(result.data);
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
-        console.log(vinoData)
-        return (
-            <ul className={'wine-card__container'}>
-                {vinoData.wines.map(item => (
-                    <li key={item.id}>
-                        <h3 className={'dark'}>{item.name}</h3>
-                        <img src={item.img} alt="item.name" />
-                        <p>{item.description}</p>
-                        <div>{item.bottleSize.ml} ml / {item.abv}% abv</div>
-                    </li>
-                ))}
-            </ul>
-        );
-    }
-}
+        const url = "/.netlify/functions/get-vinos";
+        //async fetch of Netlify function
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                setIsLoaded(true);
+                setVinoData(json)
+                return json;
+            } catch (error) {
+                setIsLoaded(true);
+                setError(error);
+                return error;
+            }
+        };
+        fetchData();
+        // Note: the empty deps array [] means
+        // this useEffect will run once
+        // similar to componentDidMount()
+    }, []);
 
+
+    return (
+        <div>placeholder</div>
+    )
+}
 export default WineList
+
+// import React, { useState, useEffect } from 'react';
+
+// const WineList = (props) => {
+//     const [error, setError] = useState(null);
+//     const [isLoaded, setIsLoaded] = useState(false);
+//     const [vinoData, setVinoData] = useState([]);
+//     // Note: the empty deps array [] means
+//     // this useEffect will run once
+//     // similar to componentDidMount()
+//     useEffect(() => {
+//         fetch("/.netlify/functions/get-vinos", {
+//             mode: 'no-cors'
+//         })
+//             .then(res => res.json())
+//             .then(
+//                 (result) => {
+//                     setIsLoaded(true);
+//                     setVinoData(result.data);
+//                 },
+//                 // Note: it's important to handle errors here
+//                 // instead of a catch() block so that we don't swallow
+//                 // exceptions from actual bugs in components.
+//                 (error) => {
+//                     setIsLoaded(true);
+//                     setError(error);
+//                 }
+//             )
+//     }, [])
+//     if (error) {
+//         return <div>Error: {error.message}</div>;
+//     } else if (!isLoaded) {
+//         return <div>Loading...</div>;
+//     } else {
+//         console.log(vinoData)
+//         return (
+//             <ul className={'wine-card__container'}>
+//                 {vinoData.wines.map(item => (
+//                     <li key={item.id}>
+//                         <h3 className={'dark'}>{item.name}</h3>
+//                         <img src={item.img} alt="item.name" />
+//                         <p>{item.description}</p>
+//                         <div>{item.bottleSize.ml} ml / {item.abv}% abv</div>
+//                     </li>
+//                 ))}
+//             </ul>
+//         );
+//     }
+// }
+
+// export default WineList
--- src/hooks/useVinoshipperApi.js
+++ src/hooks/useVinoshipperApi.js
@@ -1,5 +1,6 @@
-const useHackerNewsApi = () => {
-    const [data, setData] = useState({ hits: [] });
+import React, { useState, useEffect } from 'react';
+const useVinoshipperApi = () => {
+    const [data, setData] = useState();
     const [url, setUrl] = useState(
         '/.netlify/functions/get-vinos',
     );
@@ -7,23 +8,45 @@ const useHackerNewsApi = () => {
     const [isError, setIsError] = useState(false);
 
     useEffect(() => {
-        const fetchData = async () => {
-            setIsError(false);
-            setIsLoading(true);
-
-            try {
-                const result = await axios(url);
-
-                setData(result.data);
-            } catch (error) {
-                setIsError(true);
-            }
-
-            setIsLoading(false);
-        };
-
-        fetchData();
-    }, [url]);
-
-    return [{ data, isLoading, isError }, setUrl];
+        let fetchData = async () => {
+            console.log(url)
+            let response = await fetch(url);
+            let result = await response.json();
+            console.log(result);
+            setData(result);
+        }
+    });
+    return data;
 }
+export default useVinoshipperApi;
+
+
+// const useHackerNewsApi = () => {
+//     const [data, setData] = useState({ hits: [] });
+//     const [url, setUrl] = useState(
+//         '/.netlify/functions/get-vinos',
+//     );
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [isError, setIsError] = useState(false);
+
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             setIsError(false);
+//             setIsLoading(true);
+
+//             try {
+//                 const result = await axios(url);
+
+//                 setData(result.data);
+//             } catch (error) {
+//                 setIsError(true);
+//             }
+
+//             setIsLoading(false);
+//         };
+
+//         fetchData();
+//     }, [url]);
+
+//     return [{ data, isLoading, isError }, setUrl];
+// }
